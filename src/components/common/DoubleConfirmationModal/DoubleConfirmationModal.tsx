e09c6a5843b9cedbac1089a4b2c529aac3a98fc9
Bokhtyer Abid <68472916+bokhtyer@users.noreply.github.com>
2025-07-08 12:21:29 +0600

    tailwind

--- src/components/common/DoubleConfirmationModal/DoubleConfirmationModal.tsx
+++ src/components/common/DoubleConfirmationModal/DoubleConfirmationModal.tsx
@@ -1,49 +1,22 @@
 import Button from "../button/Button";
-import ModalBootstrap from "../modalBootstrap/ModalBootstrap";
 import "./DoubleConfirmationModal.scss";
 
 function DoubleConfirmationModal(props: any) {
+    const { isOpen, onClose, onConfirm } = props;
+    const handleConfirm = () => {
+        onConfirm();
+        onClose();
+    };
     return (
-        <div>
-            <ModalBootstrap
-                show={props.open}
-                handleClose={() => {
-                    props.close(false);
-                }}
-                size="md"
-                class="double-confirmation"
-            >
-                <div className="modal-body">
-                    {props.icon && (
-                        <div
-                            className="icon"
-                            style={{
-                                color: props.iconColor || "#FF0000",
-                            }}
-                        >
-                            {props.icon}
-                        </div>
-                    )}
-                    <p>{props.text}</p>
-                </div>
-                <div className="modal-footer justify-content-center">
-                    <Button
-                        type="submit"
-                        onClick={() => props.close(false)}
-                        btnText="Cancel"
-                        disabled={props.loading}
-                        variant="btn-cancel"
-                    />
-                    <Button
-                        type="submit"
-                        onClick={props.submit}
-                        isLoading={props.loading}
-                        disabled={props.loading || props.disabled}
-                        btnClassName={props.className}
-                        btnText={props.btnText || "Confirm"}
-                    />
-                </div>
-            </ModalBootstrap>
+        <div className="double-confirmation-modal" style={{ display: isOpen ? "block" : "none" }}>
+            Create Modal
+            <div className="modal-body">
+                <p>Are you sure you want to proceed?</p>
+            </div>
+            <div className="modal-footer">
+                <Button onClick={handleConfirm} btnText="Confirm" />
+                <Button onClick={onClose} btnText="Cancel" />
+            </div>
         </div>
     );
 }
